410d791bf52aa0f1d9e32f3b547b840af0201712
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-06-06 13:20:37 +0530

    SignUp+Login Page Text Bigger

--- frontend/src/pages/Signup/Signup.jsx
+++ frontend/src/pages/Signup/Signup.jsx
@@ -110,12 +110,11 @@ const Signup = () => {
       <div className="flex flex-col md:flex-row items-center justify-between px-6 md:px-20 pt-24 md:pt-28 pb-10 md:pb-16 gap-10 md:gap-0">
         {/* LEFT */}
         <div className="max-w-md mx-auto md:mx-0 text-center md:text-left px-2">
-          <h1 className="text-[38px] md:text-[52px] font-semibold text-teal-900 leading-[1.15]">
-            Your Wellness <br />
-            <span className="text-[#E27BA3]">Journey Begins</span>
-          </h1>
-
-          <p className="mt-4 text-[#6B7280] text-[14px] leading-[1.6] max-w-[420px] mx-auto md:mx-0">
+         <h1 className="text-[38px] md:text-[52px] font-semibold text-teal-900 leading-[1.15]">
+  Your Wellness <br />
+  <span className="text-[#E27BA3]">Journey Begins</span>
+</h1>
+          <p className="mt-4 text-[#6B7280] text-[20px] leading-[1.6] max-w-[420px] mx-auto md:mx-0">
             Join thousands building sustainable health habits through
             expert-guided programs
           </p>

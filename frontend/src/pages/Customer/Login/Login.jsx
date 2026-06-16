68ba32df78bb298c26712422d50a0d744696be57
Ravindra Kumar <ravindrakumar15075@gmail.com>
2026-06-16 18:15:44 +0530

    Added Whatsapp floating icon with pulse effect + tool tip in every 5 sec

--- frontend/src/pages/Customer/Login/Login.jsx
+++ frontend/src/pages/Customer/Login/Login.jsx
@@ -376,7 +376,7 @@ const Login = () => {
       </div>
 
       {/* 💬 WHATSAPP FLOATING BUTTON */}
-      <a
+      {/* <a
         href="https://wa.me/919876543210"
         target="_blank"
         rel="noopener noreferrer"
@@ -395,7 +395,7 @@ const Login = () => {
         <WhatsAppIcon />
 
         <span className="hidden sm:inline">Chat With Us !</span>
-      </a>
+      </a> */}
     </div>
   );
 };
